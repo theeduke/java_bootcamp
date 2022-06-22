8d3b617565316f4a674c53659420f580a63c2ffd
theeduke <oscarian528@gmail.com>
2022-06-22 16:33:42 +0300

    introduction to the bootcamp

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function disp1(){
+    
+    let rsl = document.getElementById('result');
+    let txt = document.getElementById('n1');
+
+
+    rsl.innerHTML = 
+         'Hello ' +txt.value + ', welcome to boot camp';
+}
